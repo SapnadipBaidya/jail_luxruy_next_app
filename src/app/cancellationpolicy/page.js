663bfa07c085752a31a8d2f08cbad12f9cc4d52b
Abhishek jana <abhishekjana@Abhisheks-MacBook-Pro.local>
2025-02-05 02:42:56 +0530

    new pages footer link

---
+++ src/app/cancellationpolicy/page.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PolicyLayout from "@/components/pageClients/policyLayout";
+
+const cancellationContent = [
+  {
+    title: "Cancellation Policy",
+    paragraphs: [
+      "At Jail.Luxury, we strive to ensure that our customers are completely satisfied with their purchases.",
+      "To make the process as smooth as possible, we offer an open box delivery service. This means that you have the opportunity to inspect the product at the time of delivery.",
+      "If you find that the product does not meet your expectations during this open box delivery, you may cancel the order free of charge.",
+      
+      "Please note, however, that if the product is delivered and a few hours or more have passed before you decide that it is not to your liking, our cancellation and refund policies will no longer be applicable.",
+    ],
+  },
+];
+
+const CancellationPolicy = () => {
+  return <PolicyLayout title="Cancellation Policy" content={cancellationContent} />;
+};
+
+export default CancellationPolicy;
---
+++ src/app/returnrefund/page.js
@@ -0,0 +1,43 @@
+import React from "react";
+import PolicyLayout from "@/components/pageClients/policyLayout";
+
+const returnRefundContent = [
+  {
+    title: "Return Policy",
+    paragraphs: [
+      "If you are not satisfied with your purchase, you may return the product within 5 days of delivery.",
+      "To be eligible for a return, the item must be unused, in its original packaging, and in the same condition that you received it.",
+    ],
+  },
+  {
+    title: "Refund Policy",
+    paragraphs: [
+      "Once we receive and inspect your return, we will notify you of the approval or rejection of your refund.",
+      "If approved, your refund will be processed, and a credit will be applied to your original method of payment within 10 days.",
+    ],
+  },
+  {
+    title: "Non-Returnable Items",
+    paragraphs: [
+      "Certain items, such as personalized or custom-made products, are not eligible for returns.",
+    ],
+  },
+  {
+    title: "How to Initiate a Return",
+    paragraphs: [
+      "To initiate a return, please contact our customer service at aadityaa.kumar3@gmail.com with your order details.",
+    ],
+  },
+  {
+    title: "Changes to this Policy",
+    paragraphs: [
+      "We reserve the right to update this policy at any time. Any changes will be posted on this page, and your continued use of the website constitutes acceptance of the updated policy.",
+    ],
+  },
+];
+
+const ReturnRefund = () => {
+  return <PolicyLayout title="Return and Refund Policy" content={returnRefundContent} />;
+};
+
+export default ReturnRefund;
