7949851866715391ea83d72249d44416ccf811da
Abhishek jana <abhishekjana@Abhisheks-MacBook-Pro.local>
2025-02-14 02:40:49 +0530

    navbar nav toggle

--- src/app/configs/navigationConfig.jsx
+++ src/app/configs/navigationConfig.jsx
@@ -5,100 +5,98 @@ import StyledIcon from '@/components/wrappers/StyledIcon';
 
 export const navigationConfig = [
   {
-    label: "Home",
-    path: '/',
-    component: 'Home',
-    render:false
-  },
-  {
-    label: <StyledIcon icon={ShoppingCartOutlinedIcon} />,
-    path: '/cart',
-    component: 'Cart',
-    render:true
-  },
-  {
     label: <StyledIcon icon={FavoriteBorderOutlinedIcon} />,
+    text:"Wishlist",
     path: '/wishlist',
     component: 'WishList',
     render:true
   },
   {
+    label: <StyledIcon icon={ShoppingCartOutlinedIcon} />,
+    text:"Cart",
+    path: '/cart',
+    component: 'Cart',
+    render:true
+  },
+  {
     label: "Orders",
+    text:"Orders",
     path: '/orders',
     component: 'Orders',
     render:false
   },
   {
-    label: "Track Orders",
-    path: '/track-orders',
-    component: 'TrackOrders',
+    label: "Home",
+    text:"Home",
+    path: '/',
+    component: 'Home',
     render:false
   },
   {
     label: "Login Signup",
+    text:"Login Signup",
     path: '/login-signup',
     component: 'LoginSignup',
     render:false
   },
   {
-    label: "Items",
-    path: '/product-category',
-    component: 'Items',
-    render:false
-  },
-  {
-    label: "Product",
-    path: '/product',
-    component: 'Product',
-    render:false
-  },
-  {
-    label: "User Contact",
-    path: "usercontact",
-    component: 'UserContact',
+    label:"About Us",
+    text:"About Us",
+    path: '/about-us',
+    component: 'AboutUs',
     render:false
   },
   {
     label: "Contact Us",
+    text:"Contact Us",
     path: "contact",
     component: 'ContactUs',
     render:false
   },
   {
     label: "Term Condition",
+    text:"Term Condition",
     path: "termcondition",
     component: 'TermCondition',
     render:false
   },
   {
     label: "Privacy Policy",
+    text:"Privacy Policy",
     path: "privacypolicy",
     component: 'PrivacyPolicy',
     render:false
   },
   {
     label: "Return Refund",
+    text:"Return and Refund Policy",
     path: "returnrefund",
     component: 'ReturnRefund',
     render:false
   },
   {
     label: "Shipping Policy",
+    text: "Shipping Policy",
     path: "shippingpolicy",
     component: 'ShippingPolicy',
     render:false
   },
   {
     label: "Cancellation Policy",
+    text: "Cancellation Policy",
     path: "cancellationpolicy",
     component: 'CancellationPolicy',
     render:false
-  },{
-    label: "About Us",
-    path: "aboutus",
-    component: 'AboutUs',
-    render:false
   }
+
+
+
+
+
+
+
+
+
   
   
 ];
